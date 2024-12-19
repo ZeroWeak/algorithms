7f049b404fd4cdbdb6d2db5636655d9b9f890113
eq19 <eq19@users.noreply.github.com>
2024-12-19 14:12:24 +0000

    update workflows

--- .github/entrypoint/test.php
+++ .github/entrypoint/test.php
@@ -4,13 +4,18 @@
     $secretKey = '{ACCESS_KEY}';
     
 	$data = [
-	        'method' => 'getInfo',
-	        'timestamp' => '1578304294000',
-	        'recvWindow' => '1578303937000'
-	    ];
+        'method' => 'trade',
+        'timestamp' => '1578304294000',
+        'recvWindow' => '1578303937000',
+        'pair' => 'btc_idr',
+        'type' => 'sell',
+        'price' => '1700000000',
+        'idr' => '',
+        'btc' => '0.00000863'
+    ];
 	$post_data = http_build_query($data, '', '&');
     $sign = hash_hmac('sha512', $post_data, $secretKey);
-    
+
     $headers = ['Key:'.$key,'Sign:'.$sign];
 
     $curl = curl_init();
