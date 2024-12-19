59ca5d4a1177f1e5f89b55ff5ec864b5260cabb9
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-20 00:16:16 +0700

    Update cc_exchanges.py

--- web_programming/cc_exchanges.py
+++ web_programming/cc_exchanges.py
@@ -6,7 +6,7 @@ import ccxt
 print(ccxt.exchanges)
 hitbtc   = ccxt.hitbtc({'verbose': True})
 bitmex   = ccxt.bitmex()
-huobipro = ccxt.huobipjp()
+huobipro = ccxt.huobi()
 exmo     = ccxt.indodax({
     'apiKey': os.environ['ACCESS_API'],
     'secret': os.environ['ACCESS_KEY'],
