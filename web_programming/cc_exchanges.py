26222c81474b8acc00e377c5689ae4a28c054461
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-21 18:37:19 +0700

    Update cc_exchanges.py

--- web_programming/cc_exchanges.py
+++ web_programming/cc_exchanges.py
@@ -42,12 +42,14 @@ class LSTMModel(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, num_layers):
         super(LSTMModel, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.dropout = nn.Dropout(0.2)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         h_0 = torch.zeros(1, x.size(0), hidden_size)  # Initial hidden state
         c_0 = torch.zeros(1, x.size(0), hidden_size)  # Initial cell state
         output, _ = self.lstm(x, (h_0, c_0))
+        output = self.dropout(output)
         output = self.fc(output[:, -1, :])
         return output
 
