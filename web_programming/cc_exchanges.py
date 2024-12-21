5f37a1c2f0a8d910005fab5ee1bf14a36142018e
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-21 20:21:34 +0700

    Update cc_exchanges.py

--- web_programming/cc_exchanges.py
+++ web_programming/cc_exchanges.py
@@ -48,7 +48,8 @@ class CryptoDataset(Dataset):
         self.window_size = window_size
 
     def __len__(self):
-        return len(self.data) - self.window_size
+        # Ensure the length is non-negative
+        return max(0, len(self.data) - self.window_size)
 
     def __getitem__(self, idx):
         x = self.data[idx:idx + self.window_size, :-1]
