ed4e84a1210dc38d4153bb6c9055c880483da115
eq19 <eq19@users.noreply.github.com>
2025-03-29 15:25:04 +0000

    update workflows

--- .github/entrypoint/init.sh
+++ .github/entrypoint/init.sh
@@ -77,9 +77,8 @@ if [[ "${JOBS_ID}" == "1" ]]; then
       echo "Invalid JSON"
     fi
 
-    if [[ "${RERUN_RUNNER}" != "false" ]]; then gh variable set RERUN_RUNNER --body "false"; fi
-    cd $1 && javac -d dataFile/user_data/ft_client/test_client javaCode/Main.java
     cd ${GITHUB_WORKSPACE} && mv -f $1/dataFile/user_data . && ls -al . 
+    if [[ "${RERUN_RUNNER}" != "false" ]]; then gh variable set RERUN_RUNNER --body "false"; fi
 
   fi
 
