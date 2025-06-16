9b521ad944248c6e297c65aaf6e57080aa217714
eq19 <eq19@users.noreply.github.com>
2025-06-16 04:47:21 +0000

    update workflows

--- .github/entrypoint/artifact.sh
+++ .github/entrypoint/artifact.sh
@@ -33,11 +33,11 @@ set_target() {
   
   # Iterate the Structure
   printf -v array_str -- ',,%q' "${array[@]}"
+  echo $array_str
   if [[ ! "${array_str},," =~ ",,$1,," ]]; then
     SPAN=0; echo ${array[0]}
   elif [[ "${array[-1]}" == "$1" ]]; then
     SPAN=${#array[@]}; echo $2 | sed "s|${OWNER}.github.io|${ENTRY}.github.io|g"
-echo "SPAN1=$SPAN"
     if [[ -n "$CELL" ]]; then
       if [[ "${ENTRY}" == "eq19" ]]; then
         echo "maps, feed, lexer, parser, syntax, grammar" > ${RUNNER_TEMP}/pinned_repos
@@ -53,7 +53,6 @@ echo "SPAN1=$SPAN"
         SPAN=$(( $i + 1 )); echo ${array[$SPAN]}
       fi
     done
-echo "SPAN2=$SPAN"
   fi
   
   # Generate id from the Structure
